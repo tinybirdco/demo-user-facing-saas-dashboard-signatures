1ecf9d3fd1ab869819c54befd9ac30aad55e81e7
Cameron Archer <106344464+tb-peregrine@users.noreply.github.com>
2023-09-06 10:56:21 -0500

    Update totalActivitiesPerHour.js

--- src/app/components/totalActivitiesPerHour.js
+++ src/app/components/totalActivitiesPerHour.js
@@ -1,6 +1,6 @@
 "use client";
 
-import {Card, Title, Subtitle, AreaChart, Text} from '@tremor/react';
+import { Card, Title, Subtitle, AreaChart, Text } from '@tremor/react';
 import React, { useState, useEffect } from 'react';
 import { fetchTinybirdUrl, totalActivitiesPerHourURL } from '../services/tinybirdAPIs'
 
