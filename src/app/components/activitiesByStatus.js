c69a1c937df402b792b643e82466231f55148e43
Cameron Archer <106344464+tb-peregrine@users.noreply.github.com>
2023-09-06 10:55:47 -0500

    Update activitiesByStatus.js

--- src/app/components/activitiesByStatus.js
+++ src/app/components/activitiesByStatus.js
@@ -1,6 +1,6 @@
 "use client";
 
-import {Card, Title, Subtitle, BarList, Text, Flex} from '@tremor/react';
+import { Card, Title, Subtitle, BarList, Text, Flex } from '@tremor/react';
 import React, { useState, useEffect } from 'react';
 import { fetchTinybirdUrl, activitiesByStatusURL } from '../services/tinybirdAPIs';
 
